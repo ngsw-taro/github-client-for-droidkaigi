6ece2d80e102cbcd02ef9953bdaef382d00e7a12
Taro Nagasawa <taro.nagasawa@gmail.com>
2017-03-08 09:49:09 +0900

    added exercise3 answer

--- app/src/main/kotlin/sample/githubclient/model/Repository.kt
+++ app/src/main/kotlin/sample/githubclient/model/Repository.kt
@@ -1,5 +1,8 @@
 package sample.githubclient.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 /*
  * exercise 1: リポジトリクラスを作ろう
  *
@@ -25,4 +28,33 @@ class Repository(val id: Long,
                  val htmlUrl: String,
                  val stargazersCount: Int,
                  val owner: User,
-                 val language: String?)
+                 val language: String?) : Parcelable {
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Repository> = object : Parcelable.Creator<Repository> {
+            override fun createFromParcel(source: Parcel): Repository =
+                    Repository(source.readLong(),
+                            source.readString(),
+                            source.readString(),
+                            source.readString(),
+                            source.readInt(),
+                            source.readParcelable(Repository::class.java.classLoader),
+                            source.readString())
+
+            override fun newArray(size: Int): Array<out Repository?> = kotlin.arrayOfNulls(size)
+        }
+    }
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeLong(id)
+        dest.writeString(fullName)
+        dest.writeString(description)
+        dest.writeString(htmlUrl)
+        dest.writeInt(stargazersCount)
+        dest.writeParcelable(owner, flags)
+        dest.writeString(language)
+    }
+}
