2aedab17437b786f0b3372a18eeaf9eb61487609
Taro Nagasawa <taro.nagasawa@gmail.com>
2017-03-08 10:27:49 +0900

    added answer for exercise 7

--- app/src/main/kotlin/sample/githubclient/client/GithubClient.kt
+++ app/src/main/kotlin/sample/githubclient/client/GithubClient.kt
@@ -1,6 +1,10 @@
 package sample.githubclient.client
 
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
+import sample.githubclient.model.Page
+import sample.githubclient.model.Repository
 
 interface GithubClient {
     /*
@@ -12,4 +16,6 @@ interface GithubClient {
      * クエリパラメータ q: 検索ワード
      * 戻り値 RepositoryをラップしたPage
      */
+    @GET("/search/repositories")
+    fun search(@Query("q") query: String): Call<Page<Repository>>
 }
